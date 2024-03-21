532ac7121839d82da2c252a020248113704d4fd8
houssine125 <houssinebenarous48@gmail.com>
2024-03-21 22:16:22 +0200

    paiment stripe

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -7,11 +7,15 @@ use App\Entity\Commande;
 use App\Service\PDFGeneratorService;
 use Symfony\Component\Routing\RouterInterface;
 
-use Symfony\Component\HttpFoundation\Request;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 class ProduitController extends AbstractController
 {
@@ -50,87 +54,7 @@ public function consulterPanier(): Response
 }
 
 
-#[Route('/valider-panier', name: 'valider_panier')]
-public function validerPanier(PDFGeneratorService $pdfGeneratorService, SessionInterface $session, RouterInterface $router): Response
-{
-    // Récupérer les éléments du panier depuis la session
-    $panier = $session->get('panier', []);
 
-    // Récupérer le gestionnaire d'entités
-    $entityManager = $this->getDoctrine()->getManager();
-
-    // Liste des produits achetés pour le PDF
-    $productsForPDF = [];
-
-    // Parcourir les éléments du panier
-    foreach ($panier as $item) {
-        // Récupérer l'ID et la quantité achetée du produit
-        $produitId = $item['produit']->getId();
-        $quantiteAchete = $item['quantite'];
-
-        // Trouver le produit dans la base de données par son ID
-        $produit = $entityManager->getRepository(Produit::class)->find($produitId);
-
-        // Si le produit existe
-        if ($produit) {
-            // Vérifier si la quantité en stock est suffisante
-            if ($produit->getQuantite() >= $quantiteAchete) {
-                // Mettre à jour la quantité et la quantité vendue du produit
-                $nouvelleQuantite = $produit->getQuantite() - $quantiteAchete;
-                $nouvelleQuantiteVendue = $produit->getQuantiteVendues() + $quantiteAchete;
-                $produit->setQuantite($nouvelleQuantite);
-                $produit->setQuantiteVendues($nouvelleQuantiteVendue);
-                $entityManager->persist($produit);
-            } else {
-                // Gérer le cas où la quantité en stock est insuffisante
-                // Par exemple, afficher un message d'erreur à l'utilisateur
-            }
-
-            // Créer une nouvelle entité Commande
-            $commande = new Commande();
-            $commande->setDateCommande(new \DateTime()); // Date et heure actuelles
-            $commande->setIdProduit($produit->getId());
-            $commande->setNom($produit->getNom());
-            $commande->setMontant($produit->getPrix() * $quantiteAchete);
-
-            // Enregistrer la commande dans la base de données
-            $entityManager->persist($commande);
-
-            // Ajouter les détails du produit à la liste des produits pour le PDF
-            $productsForPDF[] = [
-                'nom' => $produit->getNom(),
-                'quantite' => $quantiteAchete,
-                'montant' => $produit->getPrix() * $quantiteAchete,
-            ];
-        }
-    }
-
-
-     // Supprimez tous les éléments du panier de la session
-
-     // Redirigez ou renvoyez une réponse appropriée
-    // Enregistrer les modifications dans la base de données
-    $entityManager->flush();
-
-    // Générer le nom du fichier PDF avec la date actuelle et l'heure
-    $date = new \DateTime();
-    $fileName = 'facture_' . $date->format('Y-m-d_H-i-s') . '.pdf';
-
-    // Générer le contenu du PDF avec les détails des produits achetés
-    $pdfContent = $pdfGeneratorService->generatePDF($productsForPDF);
-
-    // Supprimer les produits achetés du panier dans la session
-    $session->set('panier', []);
-
-    // Retourner le PDF en tant que réponse HTTP
-    return new Response($pdfContent, 200, [
-        'Content-Type' => 'application/pdf',
-        'Content-Disposition' => 'attachment; filename="' . $fileName . '"'
-    ]);
-    
-    // Redirection vers la page des produits après le téléchargement du PDF
-    
-}
 
 
 
@@ -262,6 +186,161 @@ public function viderPanier(SessionInterface $session): Response
     // Redirigez ou renvoyez une réponse appropriée
     return $this->redirectToRoute('consulter_panier'); // Redirige vers la page du panier, par exemple
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+#[Route('/payment', name: 'payment')]
+public function index1(): Response
+{
+    return $this->render('payment/index.html.twig', [
+        'controller_name' => 'PaymentController',
+    ]);
+}
+
+
+#[Route('/checkout', name: 'checkout')]
+public function checkout($stripeSK): Response
+{
+    Stripe::setApiKey($stripeSK);
+
+    $session = Session::create([
+        'payment_method_types' => ['card'],
+        'line_items'           => [
+            [
+                'price_data' => [
+                    'currency'     => 'usd',
+                    'product_data' => [
+                        'name' => 'T-shirt',
+                    ],
+                    'unit_amount'  => 2000,
+                ],
+                'quantity'   => 1,
+            ]
+        ],
+        'mode'                 => 'payment',
+        'success_url'          => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        'cancel_url'           => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+    ]);
+
+    return $this->redirect($session->url, 303);
+}
+
+
+#[Route('/success-url', name: 'success_url')]
+public function validerPanier(PDFGeneratorService $pdfGeneratorService, SessionInterface $session, RouterInterface $router): Response
+{
+    // Récupérer les éléments du panier depuis la session
+    $panier = $session->get('panier', []);
+
+    // Récupérer le gestionnaire d'entités
+    $entityManager = $this->getDoctrine()->getManager();
+
+    // Liste des produits achetés pour le PDF
+    $productsForPDF = [];
+
+    // Parcourir les éléments du panier
+    foreach ($panier as $item) {
+        // Récupérer l'ID et la quantité achetée du produit
+        $produitId = $item['produit']->getId();
+        $quantiteAchete = $item['quantite'];
+
+        // Trouver le produit dans la base de données par son ID
+        $produit = $entityManager->getRepository(Produit::class)->find($produitId);
+
+        // Si le produit existe
+        if ($produit) {
+            // Vérifier si la quantité en stock est suffisante
+            if ($produit->getQuantite() >= $quantiteAchete) {
+                // Mettre à jour la quantité et la quantité vendue du produit
+                $nouvelleQuantite = $produit->getQuantite() - $quantiteAchete;
+                $nouvelleQuantiteVendue = $produit->getQuantiteVendues() + $quantiteAchete;
+                $produit->setQuantite($nouvelleQuantite);
+                $produit->setQuantiteVendues($nouvelleQuantiteVendue);
+                $entityManager->persist($produit);
+            } else {
+                // Gérer le cas où la quantité en stock est insuffisante
+                // Par exemple, afficher un message d'erreur à l'utilisateur
+            }
+
+            // Créer une nouvelle entité Commande
+            $commande = new Commande();
+            $commande->setDateCommande(new \DateTime()); // Date et heure actuelles
+            $commande->setIdProduit($produit->getId());
+            $commande->setNom($produit->getNom());
+            $commande->setMontant($produit->getPrix() * $quantiteAchete);
+
+            // Enregistrer la commande dans la base de données
+            $entityManager->persist($commande);
+
+            // Ajouter les détails du produit à la liste des produits pour le PDF
+            $productsForPDF[] = [
+                'nom' => $produit->getNom(),
+                'quantite' => $quantiteAchete,
+                'montant' => $produit->getPrix() * $quantiteAchete,
+            ];
+        }
+    }
+
+
+     // Supprimez tous les éléments du panier de la session
+
+     // Redirigez ou renvoyez une réponse appropriée
+    // Enregistrer les modifications dans la base de données
+    $entityManager->flush();
+
+    // Générer le nom du fichier PDF avec la date actuelle et l'heure
+    $date = new \DateTime();
+    $fileName = 'facture_' . $date->format('Y-m-d_H-i-s') . '.pdf';
+
+    // Générer le contenu du PDF avec les détails des produits achetés
+    $pdfContent = $pdfGeneratorService->generatePDF($productsForPDF);
+
+    // Supprimer les produits achetés du panier dans la session
+    $session->set('panier', []);
+
+    // Retourner le PDF en tant que réponse HTTP
+    return new Response($pdfContent, 200, [
+        'Content-Type' => 'application/pdf',
+        'Content-Disposition' => 'attachment; filename="' . $fileName . '"'
+    ]);
+    
+    // Redirection vers la page des produits après le téléchargement du PDF
+    
+}
+
+
+#[Route('/cancel-url', name: 'cancel_url')]
+public function cancelUrl(): Response
+{
+    return $this->render('payment/cancel.html.twig', []);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
     
