35848ee59b4a73be104ceedef0a6ac9306b50e23
mnajjaa <ibtihel.mnaja@esprit.tn>
2024-03-23 23:24:09 +0100

    redirection suivant role

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,11 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/admin', name: 'admin_dashboard')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+
+   
+
 }
