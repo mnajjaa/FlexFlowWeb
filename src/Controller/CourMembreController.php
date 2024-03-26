4833c08ec8977ad633422433c473e063b355d077
baha-eddine-dridi <149486357+baha-eddine-dridi@users.noreply.github.com>
2024-03-26 15:04:22 +0100

    Détails cours + afficher etat 1

--- src/Controller/CourMembreController.php
+++ src/Controller/CourMembreController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CoursRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Entity\Cours;
 
 class CourMembreController extends AbstractController
@@ -13,20 +15,34 @@ class CourMembreController extends AbstractController
     #[Route('/cours', name: 'liste_cours')]
     public function listeCours(CoursRepository $coursRepository): Response
     {
-        $cours = $coursRepository->findAll(); // Récupérer tous les cours depuis le repository
+        $cours = $coursRepository->findBy(['etat' => 1]); // Récupérer tous les cours avec un état égal à 1 depuis le repository
         // Convertir l'image BLOB en données binaires base64
         foreach ($cours as $cour) {
             // Convertir l'image BLOB en données binaires base64
             $cour->setImage(base64_encode(stream_get_contents($cour->getImage())));
         }
- 
-
+    
         return $this->render('GestionCours/listeMembre.html.twig', [
             'cours' => $cours,
         ]);
     }
 
+    #[Route('/cours/{id}', name: 'voir_cours')]
+public function voirCours(int $id, CoursRepository $coursRepository, Request $request): Response
+{
+    // Récupérer le cours depuis le référentiel en fonction de l'ID
+    $cours = $coursRepository->find($id);
 
+    // Vérifier si le cours existe
+    if (!$cours) {
+        throw new NotFoundHttpException('Cours non trouvé');
+    }
+
+    // Afficher les détails du cours dans un nouveau template
+    return $this->render('GestionCours/voirCours.html.twig', [
+        'cours' => $cours,
+    ]);
+}
 
         
 }
