b589035ad632fdd5ec62a187e32a4c549c4ed016
mnajjaa <ibtihel.mnaja@esprit.tn>
2024-03-29 01:37:45 +0100

    edit profile coach

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -35,6 +35,37 @@ class CoachController extends AbstractController
             'coach' => $user,
         ]);
     }
+
+    #[Route('/editProfile', name: 'coach_edit_profile')]
+    public function editProfile(Request $request, EntityManagerInterface $entityManager, SessionInterface $session): Response
+    {
+        $user = new User();
+        
+        $email = $request->getSession()->get(Security::LAST_USERNAME);
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        
+        if (!$user) {
+            throw $this->createNotFoundException('User not found.');
+        }
+
+        
+if ($request->isMethod('POST')) {
+           
+
+
+            $user->setNom($request->request->get('nom'));
+            $user->setTelephone($request->request->get('telephone'));
+            $user->setimage($request->request->get('image'));
+            $user->setEmail($request->request->get('email'));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('coach_profile');
+        }        
+        return $this->render('coach/editProfile.html.twig', [
+            'coach' => $user,
+        ]);
+    }
 }
  /* $email=$request->getSession()->get(Security::LAST_USERNAME);
         $user=$entityManager->getRepository(User::class)->findOneBy(['email'=>$email]);*/
