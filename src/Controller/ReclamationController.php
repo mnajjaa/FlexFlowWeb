2ab22ba2f016123ae10697838fd60d58e122e8fb
maalejahmed55 <72881132+maalejahmed55@users.noreply.github.com>
2024-04-17 23:20:08 +0100

    commit 13

--- src/Controller/ReclamationController.php
+++ src/Controller/ReclamationController.php
@@ -28,4 +28,40 @@ class ReclamationController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+    // Read a reclamation
+    public function show(Reclamation $reclamation): Response
+    {
+        return $this->render('reclamation/show.html.twig', [
+            'reclamation' => $reclamation,
+        ]);
+    }
+
+    // Update a reclamation
+    public function update(Request $request, Reclamation $reclamation): Response
+    {
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('reclamation_show', ['id' => $reclamation->getId()]);
+        }
+
+        return $this->render('reclamation/update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // Delete a reclamation
+    public function delete(Request $request, Reclamation $reclamation): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$reclamation->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($reclamation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('reclamation_index');
+    }
 }
