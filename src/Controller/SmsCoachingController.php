65103446660e6d985911ec438484e003792f0a0a
farah699 <farah.hmida@esprit.tn>
2024-05-07 00:48:39 +0100

    push apres integration

--- src/Controller/SmsCoachingController.php
+++ src/Controller/SmsCoachingController.php
@@ -14,13 +14,7 @@ use Doctrine\ORM\EntityManagerInterface;
 class SmsCoachingController extends AbstractController
 {      
     #[Route('/sms/coaching', name: 'app_sms_coaching')]
-    public function index(): Response
-    {
-        return $this->render('sms_coaching/index.html.twig', [
-            'controller_name' => 'SmsCoachingController',
-        ]);
-    }
-    
+   
     public function sms(SmsGenerator $SmsGenerator, \Twig\Environment $twig): Response
     {
         // Numéro de téléphone du destinataire
@@ -43,51 +37,4 @@ class SmsCoachingController extends AbstractController
         // Retourner "ok" en réponse
         return new Response("ok");
     }
-
-    private $twilioAccountSid = 'AC8802c2e9768e4876ace30c6beb9ba980'; // Remplacez par votre SID Twilio
-   private $twilioAuthToken = '6ecb828326a724faf557c7f259f06ef5'; // Remplacez par votre jeton d'authentification Twilio
-   private $twilioPhoneNumber = '+19497102963'; // Remplacez par votre numéro de téléphone Twilio
-
-    public function sendSMS(int $id, EntityManagerInterface $entityManager)
-    {
-        // Initialiser le client Twilio
-        $twilioClient = new Client($this->twilioAccountSid, $this->twilioAuthToken);
-
-        // Trouver la réservation basée sur l'ID
-        $reservation = $entityManager->getRepository(Reservation::class)->find($id);
-
-        // Vérifier si la réservation existe
-        if (!$reservation) {
-            throw $this->createNotFoundException('La réservation avec l\'ID ' . $id . ' n\'existe pas.');
-        }
-
-        // Récupérer les informations du participant, de l'événement et de la date de réservation
-        $nomParticipant = $reservation->getNomParticipant();
-        $nomEvenement = $reservation->getNomEvenement();
-        $dateReservation = $reservation->getDateReservation()->format('Y-m-d');
-
-        // Numéro de téléphone de destination
-        $toPhoneNumber = '+21653602680'; // Numéro de téléphone réel
-
-        // Corps du message SMS
-        $messageBody = sprintf(
-            "Bienvenue %s à l'événement %s. Votre réservation est confirmée  le %s.",
-            $nomParticipant,
-            $nomEvenement,
-            $dateReservation
-        );
-
-        // Envoyer le message SMS
-        $message = $twilioClient->messages->create(
-            $toPhoneNumber,
-            [
-                'from' => $this->twilioPhoneNumber,
-                'body' => $messageBody
-            ]
-        );
-
-        // Afficher le SID du message envoyé
-        echo 'Message SID: ' . $message->sid;
-    }
-
 }
